e628def2e412f4ee59f93c0f216992bc0eaf0f5b
mhdsyafar <syafarm28@gmail.com>
2025-05-26 21:45:27 +0700

    Membuat tampilan navabar dan halaman menu pizza hut

---
+++ wpu-hut/latihan1.php
@@ -0,0 +1,39 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>WPU Hut</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo.png" width="120">       
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="#">All Menu</a>
+        </div>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+
+    <div class="row mt-3">
+        <div class="col">
+            <h1>All Menu</h1>
+        </div>
+    </div>
+</div>
+
+  </body>
+</html>
+
