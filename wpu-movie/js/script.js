1967df0a64cdbfa0f61eca4e95e194726632a49a
mhdsyafar <syafarm28@gmail.com>
2025-05-27 23:15:24 +0700

    membuat fungsi detail film

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -52,3 +52,45 @@ $('#search-input').on('keyup', function(e){
     }
 });
 
+
+$('#movie-list').on('click', '.see-detail', function(){
+
+    $.ajax({
+        url: 'http://omdbapi.com',
+        type: 'GET',
+        dataType: 'json',
+        data : {
+            'apikey': '546dc95a',
+            'i': $(this).data('id')
+        },
+        success: function(movie) {
+            if (movie.Response == "True") {
+
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="`+ movie.Poster + `" class="img-fluid">
+                            </div>
+
+                            <div class="col-md-8">
+                                <ul class="list-group">
+                                    <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
+                                    <li class="list-group-item">Released : `+ movie.Released +`</li>
+                                    <li class="list-group-item">Genre : `+ movie.Genre +`</li>
+                                    <li class="list-group-item">Director : `+ movie.Director +`</li>
+                                    <li class="list-group-item">Actors : `+ movie.Actors +`</li>
+                                    
+                                    </ul>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+
+
+            }
+        }
+    
+    });
+
+});
