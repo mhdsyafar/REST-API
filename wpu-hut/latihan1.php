2539fd7d4694f52668775297b5c1746f5c0afb1a
mhdsyafar <syafarm28@gmail.com>
2025-05-27 22:33:46 +0700

    menampilkan data menu dari data json dan gambar

--- wpu-hut/latihan1.php
+++ wpu-hut/latihan1.php
@@ -1,3 +1,10 @@
+<?php
+$data = file_get_contents('data/pizza.json');
+$menu = json_decode($data, true);
+
+$menu =$menu["menu"];
+?>
+
 
 <!doctype html>
 <html lang="en">
@@ -32,8 +39,32 @@
             <h1>All Menu</h1>
         </div>
     </div>
+
+    <div class="row">
+        <?php foreach ($menu as $row) : ?>
+        <div class="col-md-4">
+            <div class="card mb-3">
+                <img src="img/menu/<?= $row["gambar"]; ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?= $row["nama"]; ?></h5>
+                    <p class="card-text"><?= $row["deskripsi"]; ?></p>
+                    <h5 class="card-title">Rp. <?= $row["harga"]; ?></h5>
+                    <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+
+
 </div>
 
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.min.js" integrity="sha384-VQqxDN0EQCkWoxt/0vsQvZswzTHUVOImccYmSyhJTp7kGtPed0Qcx8rK9h9YEgx+" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   </body>
 </html>
 
